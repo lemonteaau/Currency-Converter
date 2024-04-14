9eebe4181e129950a05636b853215cb7f9e97f22
Terry Cheng <cyr062599@gmail.com>
2024-04-14 13:32:06 +0930

    Add .prettierrc and tailwind.config.js to .gitignore Remove styles.css Import input.css in index.js Update devDependencies in package.json Remove theme-color meta tag from index.html

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ export default function App() {
         setIsLoading(true);
 
         const res = await fetch(
-          `https://api.frankfurter.app/latest?amount=${amount}&from=${currToConvert}&to=${currConverted}`
+          `https://api.frankfurter.app/latest?amount=${amount}&from=${currToConvert}&to=${currConverted}`,
         );
         const data = await res.json();
 
@@ -37,26 +37,44 @@ export default function App() {
         currencyConvert();
       }
     },
-    [amount, currToConvert, currConverted]
+    [amount, currToConvert, currConverted],
   );
 
   return (
-    <div>
-      <Input
-        amount={amount}
-        currToConvert={currToConvert}
-        currConverted={currConverted}
-        onAmountChange={handleAmountChange}
-        onSetCurrToConver={handleCurrToConvert}
-        onSetCurrConverted={handleCurrConverted}
-        isLoading={isLoading}
-      />
-      <Output
-        currConverted={currConverted}
-        convertedAmount={convertedAmount}
-        isLoading={isLoading}
-      />
-    </div>
+    <>
+      <div class="relative flex min-h-screen flex-col justify-center overflow-hidden bg-gray-50 py-6 sm:py-12">
+        <div class="relative bg-white px-6 pb-8 pt-10 shadow-xl ring-1 ring-gray-900/5 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10">
+          <div class="divide-y divide-gray-300/50">
+            <Header />
+
+            <Input
+              amount={amount}
+              currToConvert={currToConvert}
+              currConverted={currConverted}
+              onAmountChange={handleAmountChange}
+              onSetCurrToConver={handleCurrToConvert}
+              onSetCurrConverted={handleCurrConverted}
+              isLoading={isLoading}
+            />
+            <Output
+              currConverted={currConverted}
+              convertedAmount={convertedAmount}
+              isLoading={isLoading}
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+function Header() {
+  return (
+    <>
+      <header class="py-8 text-center text-2xl font-bold">
+        Currency Converter
+      </header>
+    </>
   );
 }
 
@@ -70,34 +88,39 @@ function Input({
   isLoading,
 }) {
   return (
-    <>
-      <input
-        type="text"
-        value={amount}
-        onChange={onAmountChange}
-        disabled={isLoading}
-      />
-      <select
-        value={currToConvert}
-        onChange={(e) => onSetCurrToConver(e.target.value)}
-        disabled={isLoading}
-      >
-        <option value="USD">USD</option>
-        <option value="AUD">AUD</option>
-        <option value="CNY">CNY</option>
-        <option value="SGD">SGD</option>
-      </select>
-      <select
-        value={currConverted}
-        onChange={(e) => onSetCurrConverted(e.target.value)}
-        disabled={isLoading}
-      >
-        <option value="USD">USD</option>
-        <option value="AUD">AUD</option>
-        <option value="CNY">CNY</option>
-        <option value="SGD">SGD</option>
-      </select>
-    </>
+    <div class="space-y-6 py-8 text-center text-lg leading-7 text-gray-600">
+      <div className="flex items-center justify-center space-x-4">
+        <input
+          className="rounded-md border border-gray-300 px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          type="text"
+          value={amount}
+          onChange={onAmountChange}
+          disabled={isLoading}
+        />
+        <select
+          className=" rounded-md border border-gray-300 px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          value={currToConvert}
+          onChange={(e) => onSetCurrToConver(e.target.value)}
+          disabled={isLoading}
+        >
+          <option value="USD">USD</option>
+          <option value="AUD">AUD</option>
+          <option value="CNY">CNY</option>
+          <option value="SGD">SGD</option>
+        </select>
+        <select
+          className="rounded-md border border-gray-300 px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          value={currConverted}
+          onChange={(e) => onSetCurrConverted(e.target.value)}
+          disabled={isLoading}
+        >
+          <option value="USD">USD</option>
+          <option value="AUD">AUD</option>
+          <option value="CNY">CNY</option>
+          <option value="SGD">SGD</option>
+        </select>
+      </div>
+    </div>
   );
 }
 
@@ -107,8 +130,10 @@ function Output({ currConverted, convertedAmount, isLoading }) {
   }
 
   return (
-    <p>
-      {convertedAmount} {currConverted}
-    </p>
+    <div class="pt-10 text-center text-xl font-semibold leading-7">
+      <p class="text-sky-500 hover:text-sky-600">
+        {convertedAmount} {currConverted}
+      </p>
+    </div>
   );
 }
